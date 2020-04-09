024370676b0d36402abf5beed3f93597a81acf84
dmarsden01 <david.marsden@postgrad.manchester.ac.uk>
2020-04-09 18:03:43 +0100

    Edited utility_functions to allow for backgrounds

--- utility_functions.h
+++ utility_functions.h
@@ -26,6 +26,23 @@ public:
 	void initalise_scintillation_function(const double t_singlet, const double t_triplet, const double scint_time_window, const double particle_type);
 	double get_scintillation_time() { return fScintillation_function->GetRandom(); }
 
+        // Spectrum Function (Beta Decay) /////chrisflynn
+        static double SpectrumFunction(double *x, double *par);
+
+        //Supernova Spectrum
+        static double fsn(double *x, double *par);
+
+
+        // HEP spectrum ////chrisflynn
+        static double fhep(double *x, double *par);
+
+        //Solar spectrum ////chrisflynn
+        static double fso(double *x, double *par);
+
+        //Rn Spectrum ////chrisflynn
+        static double Rn_function(double *x, double *par);
+
+
 };
 
 #endif
