bebf016bf7d95b2d24ae1912cf0f4ba060b10853
wly-wang <s2106059@phcomputeppe01.ph.ed.ac.uk>
2025-03-24 15:36:22 +0000

    work-in-progress cathode/anode separated LY plot

--- LY_plots.C
+++ LY_plots.C
@@ -34,8 +34,10 @@ void LY(const char *infilename) {
 
 	TTree *data_tree_vuv = nullptr;
 	filein->GetObject("data_tree_vuv", data_tree_vuv);
-	int data_event_vuv;
+	int data_event_vuv, data_pmt_vuv;
 	data_tree_vuv->SetBranchAddress("data_event_vuv", &data_event_vuv);
+    data_tree_vuv->SetBranchAddress("data_pmt_vuv", &data_pmt_vuv);
+    //cout<<data_pmt_vuv<<endl;
 
 	TTree *event_tree = nullptr;
 	filein->GetObject("event_tree", event_tree);
@@ -46,7 +48,7 @@ void LY(const char *infilename) {
 	event_tree->SetBranchAddress("event_z_pos", &event_z_pos);
 	event_tree->SetBranchAddress("event_E", &event_E); 
 
-
+    
 	// loop through data trees counting entries to get total hits for each event
 	int number_events = event_tree->GetEntries();
 	vector<int> hits_both(number_events,0);
@@ -62,11 +64,13 @@ void LY(const char *infilename) {
 	cout << "data tree complete" << endl;
 
 	int size_data_tree_vuv = data_tree_vuv->GetEntries();
+    int number_of_opDet = 912;
+    vector<int> new_hits_vuv(number_of_opDet, 0);
 	for (int i = 0; i < size_data_tree_vuv; i++) {
 		data_tree_vuv->GetEntry(i);
 		hits_vuv[data_event_vuv]++;
+        new_hits_vuv[data_pmt_vuv]++;
 	}
-
 	cout << "vuv only data tree complete" << endl;
 
 	// int size_data_tree_vis = data_tree_vis->GetEntries();
@@ -81,32 +85,37 @@ void LY(const char *infilename) {
 	//vector<double> LY_both;
 	//vector<double> LY_vis;
 	vector<double> LY_vuv;
+    vector<double> LY_vuv_anode;
+    vector<double> LY_vuv_cathode;
 	vector<double> x_bin;
 	vector<double> err_x;
 	//vector<double> err_y_both;
 	//vector<double> err_y_vis;
 	vector<double> err_y_vuv;
+    vector<double> err_y_vuv_anode;
+    vector<double> err_y_vuv_cathode;
 
 
 	double bin_step = 20;
-	double drift_size = 359.;
+	double drift_size = 718.;
 	
 	// loop over bins in x, calculating light yield
 	for (int bin = 0; bin < drift_size/bin_step; bin++){
 		//vector<double> LY_vals_both; LY_vals_both.reserve(1e6);
 		vector<double> LY_vals_vuv; LY_vals_vuv.reserve(1e6);
+        vector<double> LY_vals_vuv_anode; LY_vals_vuv_anode.reserve(1e6);
+        vector<double> LY_vals_vuv_cathode; LY_vals_vuv_cathode.reserve(1e6);
 		//vector<double> LY_vals_vis; LY_vals_vis.reserve(1e6);
 
 		double xbin_min = bin*bin_step;
 		double xbin_max = (bin+1)*bin_step;
-		x_bin.push_back((xbin_max + xbin_min)/2);
+		x_bin.push_back((xbin_max + xbin_min)/2  - 359.);
 		err_x.push_back(bin_step/2);
-
 		// loop over events selecting those within current bin
 		for (int i = 0; i < number_events; i++){
 			event_tree->GetEntry(i);
 			// LY of events occuring within bin
-			if (event_x_pos > xbin_min && event_x_pos <= xbin_max) {
+			if (event_x_pos > xbin_min - 359. && event_x_pos <= xbin_max - 359.) {
 				//double LY_val_both = hits_both[event_no]/event_E;
 				//double LY_val_vis = hits_vis[event_no]/event_E;
 				double LY_val_vuv = hits_vuv[event_no]/event_E;
@@ -114,12 +123,19 @@ void LY(const char *infilename) {
 				//LY_vals_both.push_back(LY_val_both);
 				LY_vals_vuv.push_back(LY_val_vuv);
 				//LY_vals_vis.push_back(LY_val_vis);
+                if (data_pmt_vuv <= 479){
+                    LY_vals_vuv_anode.push_back(LY_val_vuv);
+                } else {
+                    LY_vals_vuv_cathode.push_back(LY_val_vuv);
+                }
+                
+                
 			}
 		}
-
+        cout<<LY_vals_vuv_anode.size()<<endl;
 		// calculate mean and stdev of LY for this bin for plotting 
-		double m_both, m_vuv; // ,m_vis; 
-        double stdev_both, stdev_vuv; // ,stdev_vis;		
+		double m_both, m_vuv, m_vuv_anode, m_vuv_cathode; // ,m_vis; 
+        double stdev_both, stdev_vuv, stdev_vuv_anode, stdev_vuv_cathode; // ,stdev_vis;		
 		
 		// // both
 		// // mean
@@ -139,6 +155,24 @@ void LY(const char *infilename) {
 		std::for_each (std::begin(LY_vals_vuv), std::end(LY_vals_vuv), [&](const double d) { accum_vuv += (d - m_vuv) * (d - m_vuv); } );
 		stdev_vuv = sqrt(accum_vuv / (LY_vals_vuv.size()-1));
 
+        // vuv anode
+        // mean
+        double sum_vuv_anode = std::accumulate(std::begin(LY_vals_vuv_anode), std::end(LY_vals_vuv_anode), 0.0);
+        m_vuv_anode =  sum_vuv_anode / LY_vals_vuv_anode.size();
+        // stdev
+        double accum_vuv_anode = 0.0;
+        std::for_each (std::begin(LY_vals_vuv_anode), std::end(LY_vals_vuv_anode), [&](const double d) { accum_vuv_anode += (d - m_vuv_anode) * (d - m_vuv_anode); } );
+        stdev_vuv_anode = sqrt(accum_vuv_anode / (LY_vals_vuv_anode.size()-1));
+
+        // vuv cathode
+        // mean
+        double sum_vuv_cathode = std::accumulate(std::begin(LY_vals_vuv_cathode), std::end(LY_vals_vuv_cathode), 0.0);
+        m_vuv_cathode =  sum_vuv_cathode / LY_vals_vuv_cathode.size();
+        // stdev
+        double accum_vuv_cathode = 0.0;
+        std::for_each (std::begin(LY_vals_vuv_cathode), std::end(LY_vals_vuv_cathode), [&](const double d) { accum_vuv_cathode += (d - m_vuv_cathode) * (d - m_vuv_cathode); } );
+        stdev_vuv_cathode = sqrt(accum_vuv_cathode / (LY_vals_vuv_cathode.size()-1));
+
 		// // vis
 		// // mean
 		// double sum_vis = std::accumulate(std::begin(LY_vals_vis), std::end(LY_vals_vis), 0.0);
@@ -152,17 +186,20 @@ void LY(const char *infilename) {
 		// LY_both.push_back(m_both);
 		// LY_vis.push_back(m_vis);
 		LY_vuv.push_back(m_vuv);
-		
-		//err_y_both.push_back(stdev_both);
-		// err_y_vis.push_back(stdev_vis);
-		err_y_vuv.push_back(stdev_vuv);
+		LY_vuv_anode.push_back(m_vuv_anode);
+        LY_vuv_cathode.push_back(m_vuv_cathode);
+        
+        //err_y_both.push_back(stdev_both);
+        // err_y_vis.push_back(stdev_vis);
+        err_y_vuv.push_back(stdev_vuv);
+        err_y_vuv_anode.push_back(stdev_vuv_anode);
+        err_y_vuv_cathode.push_back(stdev_vuv_cathode);
 
 	}
 
     // create output .root file
     unique_ptr<TFile> myFile(TFile::Open("LY_plots.root", "RECREATE"));
     cout << "An empty .root file created for storing output plots." << endl;
-
 	// create plot
     TCanvas *c1 = new TCanvas("c1","",200,10,1080,1080);
     auto mg = new TMultiGraph();
@@ -172,30 +209,44 @@ void LY(const char *infilename) {
 
 	// TGraph *gr1 = new TGraphErrors(x_bin.size(), &(x_bin[0]),&(LY_both[0]),&(err_x[0]),&(err_y_both[0]));
 	TGraph *gr2 = new TGraphErrors(x_bin.size(), &(x_bin[0]),&(LY_vuv[0]),&(err_x[0]),&(err_y_vuv[0]));
+    TGraph *gr3 = new TGraphErrors(x_bin.size(), &(x_bin[0]),&(LY_vuv_anode[0]),&(err_x[0]),&(err_y_vuv_anode[0]));
+    TGraph *gr4 = new TGraphErrors(x_bin.size(), &(x_bin[0]),&(LY_vuv_cathode[0]),&(err_x[0]),&(err_y_vuv_cathode[0]));
 	// TGraph *gr3 = new TGraphErrors(x_bin.size(), &(x_bin[0]),&(LY_vis[0]),&(err_x[0]),&(err_y_vis[0]));
 
 	//gr1->SetMarkerColor(1);
 	gr2->SetMarkerColor(4);
+    gr3->SetMarkerColor(2);
+    gr4->SetMarkerColor(3);
 	//gr3->SetMarkerColor(2);
 
 	//gr1->SetLineColor(1);
 	gr2->SetLineColor(4);
+    gr3->SetLineColor(2);
+    gr4->SetLineColor(3);
 	//gr3->SetLineColor(2);
 
 	//gr1->SetMarkerStyle(20);
 	gr2->SetMarkerStyle(22);
+    gr3->SetMarkerStyle(23);
+    gr4->SetMarkerStyle(24);
 	//gr3->SetMarkerStyle(23);
 
 	///gr1->SetMarkerSize(2);
 	gr2->SetMarkerSize(2);
+    gr3->SetMarkerSize(2);
+    gr4->SetMarkerSize(2);
 	//gr3->SetMarkerSize(2);
 
 	//legend->AddEntry(gr1, "Total" ,"p");
 	legend->AddEntry(gr2, "Direct" ,"p");
+    legend->AddEntry(gr3, "Anode" ,"p");
+    legend->AddEntry(gr4, "Cathode" ,"p");
 	//legend->AddEntry(gr3, "Reflected" ,"p");
 
 	//mg->Add(gr1);
 	mg->Add(gr2);
+    mg->Add(gr3);
+    mg->Add(gr4);
 	//mg->Add(gr3);
 
 	// draw graph
